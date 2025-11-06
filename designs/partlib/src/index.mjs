76757f81aa57d6f99e0b031017f146bbdb943fcf
Joost De Cock <joost@joost.at>
2025-11-06 17:02:41 +0100

    [partlib] feat: More 2-part sleeve to partlib

--- designs/partlib/src/index.mjs
+++ designs/partlib/src/index.mjs
@@ -1,13 +1,15 @@
 import { Design } from '@freesewing/core'
 import about from '../about.json' with { type: 'json' }
 import { sleeve1 } from './sleeve-1.mjs'
+import { underSleeve2 } from './undersleeve-2.mjs'
+import { topSleeve2 } from './topsleeve-2.mjs'
 import { i18n } from '../i18n/index.mjs'
 
 // Setup our new design
 const Partlib = new Design({
   data: about,
-  parts: [sleeve1],
+  parts: [sleeve1, underSleeve2, topSleeve2],
 })
 
 // Named exports
-export { sleeve1, Partlib, i18n, about }
+export { sleeve1, underSleeve2, topSleeve2, Partlib, i18n, about }
---
+++ designs/partlib/src/undersleeve-2.mjs
@@ -0,0 +1,67 @@
+import { sleeve2, dimensions } from './sleeve-2.mjs'
+
+function draftUnderSleeve2({ macro, Path, points, paths, snippets, Snippet, sa, store, part }) {
+  // Extract seamline from sleeve
+  delete paths.ts
+  paths.seam = paths.us.clone().attr('class', 'fabric', true)
+  delete paths.us
+
+  points.anchor = points.usTip.clone()
+
+  // Seam allowance
+  if (sa) {
+    paths.sa = paths.seam.clone()
+    // Remove hem
+    paths.sa.ops.splice(-2)
+    paths.sa = paths.sa
+      .offset(sa)
+      .join(
+        new Path()
+          .move(points.usWristLeft)
+          .line(points.usWristRight)
+          .offset(sa * 3)
+      )
+      .close()
+      .attr('class', 'fabric sa')
+  }
+
+  /*
+   * Annotatinos
+   */
+
+  // Cutlist
+  store.cutlist.addCut({ cut: 2, from: 'fabric' })
+
+  // Logo
+  snippets.logo = new Snippet('logo', points.elbowCenter)
+
+  // Title
+  macro('title', {
+    at: points.armCenter,
+    nr: 4,
+    title: 'undersleeve',
+  })
+
+  // Dimensions
+  dimensions(part, 'us')
+  macro('hd', {
+    id: 'wArmholeInnerSleeveCapTip',
+    from: points.usLeftEdge,
+    to: points.usTip,
+    y: points.usTip.y - sa - 15,
+  })
+  macro('vd', {
+    id: 'hArmholeInnerSleeveCapTip',
+    from: points.tsRightEdge,
+    to: points.usTip,
+    x: points.tsRightEdge.x + sa + 15,
+  })
+
+  return part
+}
+
+export const underSleeve2 = {
+  name: 'partlib.underSleeve2',
+  from: sleeve2,
+  draft: draftUnderSleeve2,
+}
