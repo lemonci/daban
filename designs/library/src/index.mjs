6dde76aa751c3975feee4fb2a93af21122489679
joostdecock <joost@joost.at>
2025-12-13 16:58:48 +0100

    [library] feat: Added rectangle

--- designs/library/src/index.mjs
+++ designs/library/src/index.mjs
@@ -2,17 +2,18 @@ import { Design } from '@freesewing/core'
 import about from '../about.json' with { type: 'json' }
 import { i18n } from '../i18n/index.mjs'
 // Parts
+import { rectangle } from './rectangle/index.mjs'
 import { sleeve } from './sleeve/index.mjs'
 import { topsleeve, undersleeve, twoPartSleeve } from './two-part-sleeve/index.mjs'
 
 // Setup our new design
 const Library = new Design({
   data: about,
-  parts: [sleeve, topsleeve, undersleeve, twoPartSleeve],
+  parts: [rectangle, sleeve, topsleeve, undersleeve, twoPartSleeve],
 })
 
 // Named exports
-export { sleeve, topsleeve, undersleeve, twoPartSleeve, Library, i18n, about }
+export { rectangle, sleeve, topsleeve, undersleeve, twoPartSleeve, Library, i18n, about }
 
 const _ = {
   needs: {
---
+++ designs/library/src/rectangle/index.mjs
@@ -0,0 +1,126 @@
+export const rectangle = {
+  library: true,
+  name: 'library.rectangle',
+  measurements: [],
+  options: {
+    seamClasses: 'fabric',
+    rectangleWidth: { pct: 100, min: 50, max: 150, menu: 'rectangle' },
+    rectangleHeight: { pct: 100, min: 50, max: 150, menu: 'rectangle' },
+    naSa: { bool: false },
+  },
+  store: {
+    reads: ['width', 'height'],
+  },
+  draft: ({
+    store,
+    sa,
+    macro,
+    snippets,
+    Snippet,
+    units,
+    options,
+    Point,
+    points,
+    Path,
+    paths,
+    log,
+    measurements,
+    part,
+  }) => {
+    // Points
+    points.topLeft = new Point(0, 0)
+    points.topRight = new Point(store.pget('width') * options.rectangleWidth, 0)
+    points.bottomRight = new Point(
+      points.topRight.x,
+      store.pget('height') * options.rectangleHeight
+    )
+    points.bottomLeft = new Point(0, points.bottomRight.y)
+
+    // Paths
+    paths.seam = new Path()
+      .move(points.topLeft)
+      .line(points.bottomLeft)
+      .line(points.bottomRight)
+      .line(points.topRight)
+      .line(points.topLeft)
+      .close()
+      .addClass(options.seamClasses)
+
+    if (sa && !options.noSa) paths.sa = paths.seam.offset(sa).attr('class', 'fabric sa')
+
+    /*
+     * Annotations
+
+    // Anchor point for sampling
+    points.gridAnchor = new Point(0, 0)
+
+    // Grainline
+    macro('grainline', {
+      from: points.centerWrist,
+      to: points.centerBiceps,
+    })
+
+    // Cut list
+    store.cutlist.addCut({ cut: 2, from: 'fabric' })
+
+    // Logo
+    points.logo = points.centerBiceps.shiftFractionTowards(points.centerWrist, 0.3)
+    snippets.logo = new Snippet('logo', points.logo)
+
+    // Title
+    macro('title', { at: points.centerBiceps, nr: 3, title: 'sleeve' })
+
+    // Scalebox
+    points.scaleboxAnchor = points.scalebox = points.centerBiceps.shiftFractionTowards(
+      points.centerWrist,
+      0.5
+    )
+    macro('scalebox', { at: points.scalebox })
+
+    // Notches
+    if (store.pget('frontArmholeToArmholePitch')) {
+      points.frontNotch = paths.sleevecap.shiftAlong(store.pget('frontArmholeToArmholePitch'))
+      snippets.frontNotch = new Snippet('notch', points.frontNotch)
+    }
+    if (store.pget('backArmholeToArmholePitch')) {
+      points.backNotch = paths.sleevecap
+        .reverse()
+        .shiftAlong(store.pget('backArmholeToArmholePitch'))
+      snippets.backNotch = new Snippet('bnotch', points.backNotch)
+    }
+
+    // Dimensions
+    macro('vd', {
+      id: 'hCuffToArmhole',
+      from: points.wristLeft,
+      to: points.bicepsLeft,
+      x: points.bicepsLeft.x - sa - 15,
+    })
+    macro('vd', {
+      id: 'hFull',
+      from: points.wristLeft,
+      to: points.sleeveTip,
+      x: points.bicepsLeft.x - sa - 30,
+    })
+    macro('hd', {
+      id: 'wFull',
+      from: points.bicepsLeft,
+      to: points.bicepsRight,
+      y: points.sleeveTip.y - sa - 30,
+    })
+    macro('hd', {
+      id: 'wCuff',
+      from: points.wristLeft,
+      to: points.wristRight,
+      y: points.wristLeft.y + sa + 30,
+    })
+    macro('pd', {
+      id: 'lSleevevap',
+      path: paths.sleevecap.reverse(),
+      d: -1 * sa - 15,
+    })
+     */
+
+    return part
+  },
+}
