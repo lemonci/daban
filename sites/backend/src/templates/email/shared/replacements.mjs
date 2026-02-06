1763f9759569e6b9c7b3a79be1b54e18dfc98f97
Joost De Cock <joost@joost.at>
2026-02-06 07:50:15 +0000

    [backend] chore: Improve patron call phrasing

--- sites/backend/src/templates/email/shared/replacements.mjs
+++ sites/backend/src/templates/email/shared/replacements.mjs
@@ -1,9 +1,9 @@
 export const replacements = {
   greeting: 'love',
-  'ps-pre-link': 'FreeSewing is free (duh), but please',
+  'ps-pre-link': 'FreeSewing is free, but please',
   'ps-link': 'become a patron',
   'ps-post-link': 'if you can afford it.',
-  'text-ps': 'FreeSewing is free (duh), but please become a patron if you can afford it.',
+  'text-ps': 'FreeSewing is free, but please become a patron if you can afford it.',
   notMarketing: 'This is not marketing, but a transactional email from FreeSewing',
   whyDidIGetThis: 'Why did I get this email?',
 }
