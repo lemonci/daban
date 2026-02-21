2d95459f729b163f1d49174e568ac8026b4e771f
Jonathan Haas <haasjona@gmail.com>
2026-02-21 13:03:19 +0100

    Sunny: Don't skip creating points and paths in base part if not complete
    These are needed by the binding part regardless of complete setting.
    Fixes #739

--- designs/sunny/src/base.mjs
+++ designs/sunny/src/base.mjs
@@ -160,62 +160,60 @@ export const base = {
     points.br = new Point(w, l)
     points.tr = new Point(w, 0)
 
-    if (complete) {
-      points.lNotch = new Point(0, o)
-      points.rNotch = new Point(w, o)
+    points.lNotch = new Point(0, o)
+    points.rNotch = new Point(w, o)
 
-      if (options.fabricWaistband) {
-        points.wfold1tr = points.wtr.shift(-90, waistbandWidth / 4)
-        points.wfold1tl = points.tl.shift(-90, waistbandWidth / 4)
-        points.wfold1br = points.wbr.shift(90, waistbandWidth / 4)
-        points.wfold1bl = points.wbl.shift(90, waistbandWidth / 4)
+    if (options.fabricWaistband) {
+      points.wfold1tr = points.wtr.shift(-90, waistbandWidth / 4)
+      points.wfold1tl = points.tl.shift(-90, waistbandWidth / 4)
+      points.wfold1br = points.wbr.shift(90, waistbandWidth / 4)
+      points.wfold1bl = points.wbl.shift(90, waistbandWidth / 4)
 
-        points.wfold2r = points.wtr.shift(-90, waistbandWidth / 2)
-        points.wfold2l = points.tl.shift(-90, waistbandWidth / 2)
+      points.wfold2r = points.wtr.shift(-90, waistbandWidth / 2)
+      points.wfold2l = points.tl.shift(-90, waistbandWidth / 2)
 
-        points.wtlNotch = new Point(b / 2 - h / 2, waistbandWidth / 4)
-        points.wtrNotch = new Point(b / 2 + h / 2, waistbandWidth / 4)
-        points.wblNotch = new Point(b / 2 - h / 2, (waistbandWidth / 4) * 3)
-        points.wbrNotch = new Point(b / 2 + h / 2, (waistbandWidth / 4) * 3)
-      }
+      points.wtlNotch = new Point(b / 2 - h / 2, waistbandWidth / 4)
+      points.wtrNotch = new Point(b / 2 + h / 2, waistbandWidth / 4)
+      points.wblNotch = new Point(b / 2 - h / 2, (waistbandWidth / 4) * 3)
+      points.wbrNotch = new Point(b / 2 + h / 2, (waistbandWidth / 4) * 3)
+    }
 
-      if (options.fabricBinding) {
-        points.bbl = new Point(0, bindingWidth)
-        points.btr = new Point(h, 0)
-        points.bbr = new Point(h, bindingWidth)
-        points.bfold1tr = points.btr.shift(-90, bindingWidth / 4)
-        points.bfold1tl = points.tl.shift(-90, bindingWidth / 4)
-        points.bfold1br = points.bbr.shift(90, bindingWidth / 4)
-        points.bfold1bl = points.bbl.shift(90, bindingWidth / 4)
+    if (options.fabricBinding) {
+      points.bbl = new Point(0, bindingWidth)
+      points.btr = new Point(h, 0)
+      points.bbr = new Point(h, bindingWidth)
+      points.bfold1tr = points.btr.shift(-90, bindingWidth / 4)
+      points.bfold1tl = points.tl.shift(-90, bindingWidth / 4)
+      points.bfold1br = points.bbr.shift(90, bindingWidth / 4)
+      points.bfold1bl = points.bbl.shift(90, bindingWidth / 4)
 
-        points.bfold2r = points.btr.shift(-90, bindingWidth / 2)
-        points.bfold2l = points.tl.shift(-90, bindingWidth / 2)
-      }
+      points.bfold2r = points.btr.shift(-90, bindingWidth / 2)
+      points.bfold2l = points.tl.shift(-90, bindingWidth / 2)
+    }
 
-      const pps = options.numPleats
-      const ws = (measurements.waist + waistEase) / 2
-      const pleatWidth = ws / options.numPleats
-      const pleatInterval = pleatWidth * (3 - options.pleatOverlap)
+    const pps = options.numPleats
+    const ws = (measurements.waist + waistEase) / 2
+    const pleatWidth = ws / options.numPleats
+    const pleatInterval = pleatWidth * (3 - options.pleatOverlap)
 
-      // pleat
-      for (let i = 0; i < pps; i++) {
-        // up/down
-        for (let j = 0; j < 2; j++) {
-          // top/bottom
-          for (let k = 0; k < 2; k++) {
-            points[`pleat${i + 1}_${k ? 'top' : 'bottom'}_${j ? 'up' : 'down'}`] = points[
-              k ? 'tl' : 'bl'
-            ].shift(0, pleatWidth * (2 - j) + pleatInterval * i)
-          }
+    // pleat
+    for (let i = 0; i < pps; i++) {
+      // up/down
+      for (let j = 0; j < 2; j++) {
+        // top/bottom
+        for (let k = 0; k < 2; k++) {
+          points[`pleat${i + 1}_${k ? 'top' : 'bottom'}_${j ? 'up' : 'down'}`] = points[
+            k ? 'tl' : 'bl'
+          ].shift(0, pleatWidth * (2 - j) + pleatInterval * i)
         }
       }
+    }
 
-      paths.upFolds = new Path().hide().addClass('lashed')
-      paths.downFolds = new Path().hide().addClass('dashed')
-      for (let i = pps; i > 0; i--) {
-        paths.upFolds.move(points[`pleat${i}_top_up`]).line(points[`pleat${i}_bottom_up`])
-        paths.downFolds.move(points[`pleat${i}_top_down`]).line(points[`pleat${i}_bottom_down`])
-      }
+    paths.upFolds = new Path().hide().addClass('lashed')
+    paths.downFolds = new Path().hide().addClass('dashed')
+    for (let i = pps; i > 0; i--) {
+      paths.upFolds.move(points[`pleat${i}_top_up`]).line(points[`pleat${i}_bottom_up`])
+      paths.downFolds.move(points[`pleat${i}_top_down`]).line(points[`pleat${i}_bottom_down`])
     }
 
     points.text = new Point(25, 50)
