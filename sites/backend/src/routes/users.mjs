f8b2e388c7bd38a0a7a72c452b09bcbefcc495b8
joostdecock <joost@joost.at>
2026-03-07 16:09:08 +0100

    [backend] fix: Disable anonymous profile data endpoint

--- sites/backend/src/routes/users.mjs
+++ sites/backend/src/routes/users.mjs
@@ -77,7 +77,8 @@ export function usersRoutes(tools) {
   )
 
   // Load a user profile
-  app.get('/users/:id', (req, res) => Users.profile(req, res, tools))
+  // We removed this to prevent user enumeration
+  //app.get('/users/:id', (req, res) => Users.profile(req, res, tools))
 
   // Export account data
   app.get('/account/export/jwt', passport.authenticate(...jwt), (req, res) =>
