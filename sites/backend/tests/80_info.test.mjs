79873c0c7a69ec947725ddd74c051e765e80f7b1
joostdecock <joost@joost.at>
2026-03-26 17:29:40 +0100

    [backend] chore: Added more unit tests

---
+++ sites/backend/tests/80_info.test.mjs
@@ -0,0 +1,21 @@
+import { api, auth, cat, store, loadStore } from './utils.mjs'
+import { strict as assert } from 'node:assert'
+import { describe, it } from 'node:test'
+
+describe(`Info tests`, () => {
+  it(`Should get the stats endpoint)`, async () => {
+    const [status, data] = await api.get('/info/stats')
+    assert.equal(status, 200)
+    assert.equal(typeof data.activity.jwt, 'number')
+    assert.equal(typeof data.activity.key, 'number')
+    assert.equal(typeof data.user, 'number')
+    assert.equal(typeof data.pattern, 'number')
+    assert.equal(typeof data.set, 'number')
+    assert.equal(typeof data.curatedSet, 'number')
+  })
+  it(`Should get the stats endpoint)`, async () => {
+    const [status, data] = await api.get('/info/users')
+    assert.equal(status, 200)
+    assert.equal(typeof data.users, 'number')
+  })
+})
