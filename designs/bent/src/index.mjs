d913cac8bb0b3e218865c0a68de2d84aa3bc2ff6
Jonathan Haas <haasjona@gmail.com>
2026-04-07 20:12:38 +0200

    [bent] fix: Restore options from before the part library change (#833)
    Bents non-sleeve-related default options got removed in the library update (together with all parts).
    
    This patch restores the options that are different from the library sleeve and Brian by introducing a dummy part.
    
    Also affects designs inheriting Bent, such as Jaeger.
    
    Reviewed-on: https://codeberg.org/freesewing/freesewing/pulls/833
    Reviewed-by: Joost De Cock <joostdecock@noreply.codeberg.org>
    Co-authored-by: Jonathan Haas <haasjona@gmail.com>
    Co-committed-by: Jonathan Haas <haasjona@gmail.com>

--- designs/bent/src/index.mjs
+++ designs/bent/src/index.mjs
@@ -1,8 +1,51 @@
-import { Design, mergeI18n } from '@freesewing/core'
+import { Design, mergeI18n, pctBasedOn } from '@freesewing/core'
 import about from '../about.json' with { type: 'json' }
-import { front, back, i18n as brianI18n } from '@freesewing/brian'
+import { front as brianFront, back as brianBack, i18n as brianI18n } from '@freesewing/brian'
 import { undersleeve, topsleeve, twoPartSleeveI18n } from '@freesewing/library'
 import { i18n as bentI18n } from '../i18n/index.mjs'
+
+// Options that are different in Bent from Brian
+const optionsOverride = {
+  // Fit
+  chestEase: { pct: 8, min: -4, max: 20, ...pctBasedOn('chest'), menu: 'fit' },
+  collarEase: { pct: 3.5, min: 0, max: 10, ...pctBasedOn('neck'), menu: 'fit' },
+  bicepsEase: { pct: 20, min: 10, max: 40, ...pctBasedOn('biceps'), menu: 'fit' },
+  cuffEase: { pct: 40, min: 2, max: 100, ...pctBasedOn('wrist'), menu: 'fit' },
+  // Advanced
+  acrossBackFactor: { pct: 97, min: 93, max: 100, menu: 'advanced' },
+  frontArmholeDeeper: { pct: 0.5, min: 0, max: 1.5, menu: 'advanced' },
+  // v3 armhole depth
+  armholeDepth: {
+    pct: 5,
+    min: -10,
+    max: 50,
+    menu: (_settings, mergedOptions) => (mergedOptions?.legacyArmholeDepth ? false : 'advanced'),
+  },
+  armholeDepthFactor: {
+    pct: 60,
+    min: 50,
+    max: 70,
+    menu: (_settings, mergedOptions) => (mergedOptions?.legacyArmholeDepth ? 'advanced' : false),
+  },
+}
+
+// We need to redefine both Brian parts with different options.
+// This is because both Brian parts define the same options, and otherwise
+// we can run into issues with options priority
+const back = {
+  ...brianBack,
+  options: {
+    ...brianBack.options,
+    ...optionsOverride,
+  },
+}
+const front = {
+  ...brianFront,
+  options: {
+    ...brianFront.options,
+    ...optionsOverride,
+  },
+}
 
 // Create new design
 const Bent = new Design({
