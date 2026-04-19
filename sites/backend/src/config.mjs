db83d3c1e5d91345e1aa9a8be13fd508d186a123
joostdecock <joost@joost.at>
2026-04-19 18:34:51 +0200

    [backend] feat: Add morio and supermorio OIDC clients

--- sites/backend/src/config.mjs
+++ sites/backend/src/config.mjs
@@ -62,6 +62,8 @@ const baseConfig = {
       clients: {
         forum: envToBool(process.env.BACKEND_ENABLE_OIDC_CLIENT_FORUM),
         support: envToBool(process.env.BACKEND_ENABLE_OIDC_CLIENT_SUPPORT),
+        morio: envToBool(process.env.BACKEND_ENABLE_OIDC_CLIENT_MORIO),
+        supermorio: envToBool(process.env.BACKEND_ENABLE_OIDC_CLIENT_SUPERMORIO),
       },
     },
     noCORS: envToBool(process.env.BACKEND_ENABLE_OPEN_API),
@@ -215,6 +217,24 @@ if (baseConfig.use.oidc.provider) {
       client_id: 'support',
       client_secret: process.env['BACKEND_OIDC_CLIENT_SUPPORT_SECRET'],
       redirect_uris: ['https://support.freesewing.eu/oauth-login/callback/avjk80n5p'],
+      grant_types: ['authorization_code'],
+      response_types: ['code'],
+      scope: 'openid email profile',
+    })
+  if (baseConfig.use.oidc.clients.morio)
+    baseConfig.oidc.provider.clients.push({
+      client_id: 'morio',
+      client_secret: process.env['BACKEND_OIDC_CLIENT_MORIO_SECRET'],
+      redirect_uris: ['https://morio.freesewing.eu/-/api/callback/oidc/morio'],
+      grant_types: ['authorization_code'],
+      response_types: ['code'],
+      scope: 'openid email profile',
+    })
+  if (baseConfig.use.oidc.clients.supermorio)
+    baseConfig.oidc.provider.clients.push({
+      client_id: 'supermorio',
+      client_secret: process.env['BACKEND_OIDC_CLIENT_SUPERMORIO_SECRET'],
+      redirect_uris: ['https://morio.freesewing.eu/-/api/callback/oidc/supermorio'],
       grant_types: ['authorization_code'],
       response_types: ['code'],
       scope: 'openid email profile',
--- sites/backend/src/utils/oidc-provider.mjs
+++ sites/backend/src/utils/oidc-provider.mjs
@@ -119,7 +119,7 @@ async function loadExistingGrant(ctx) {
   if (grantId) return ctx.oidc.provider.Grant.find(grantId)
 
   // Auto-approve consent for first-party clients
-  const firstPartyClients = ['forum', 'support']
+  const firstPartyClients = ['forum', 'support', 'morio', 'supermorio']
   if (firstPartyClients.includes(ctx.oidc.client.clientId)) {
     const grant = new ctx.oidc.provider.Grant({
       clientId: ctx.oidc.client.clientId,
